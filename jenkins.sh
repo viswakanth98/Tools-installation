339f1bec4487901b2c934b509628a4ec0b3201eb
viswakanth98 <140156745+viswakanth98@users.noreply.github.com>
2023-11-15 19:00:25 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-sudo apt update -y
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
 sudo apt update -y
 
-apt-cache policy docker-ce -y
+sudo apt upgrade -y 
 
-sudo apt install docker-ce -y
+sudo apt install openjdk-17-jre -y
 
-#sudo systemctl status docker
-
-sudo chmod 777 /var/run/docker.sock
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
